5c74c65471cd933ab518f948e49ebe37082b3d34
Idonotcode <siddhantmane@gmail.com>
2023-11-26 22:20:18 -0500

    rb insert done

--- rb_tree/src/main.rs
+++ rb_tree/src/main.rs
@@ -1,8 +1,8 @@
 use std::fmt::Debug;
+use std::process::exit;
 use std::rc::Rc;
 use std::rc::Weak;
 use std::cell::RefCell;
-
 
 pub struct RBTree<K: std::cmp::PartialOrd + Copy> {
     root: StrongNodeRef<K>
@@ -19,6 +19,7 @@ struct RBNode<K: std::cmp::PartialOrd + Copy> {
     p: WeakNodeRef<K>
 }
 
+#[derive(Copy, Clone, PartialEq)]
 enum RBColor {
     Red,
     Black
@@ -47,11 +48,7 @@ enum RBColor {
     right[z] = nil[T]
     color[z] = RED
     RB-Insert-fixup(T,z)
-
-  
-
-
- */
+*/
 
 fn clone_node<T: std::cmp::PartialOrd + Copy>(node: &StrongNodeRef<T>) -> StrongNodeRef<T> {
     node.as_ref().map(|rc| Rc::clone(&rc))
@@ -88,6 +85,48 @@ fn get_parent<T: std::cmp::PartialOrd + Copy>(node: &StrongNodeRef<T>) -> WeakNo
     }
 }
 
+fn get_color<T: std::cmp::PartialOrd + Copy>(node: &StrongNodeRef<T>) -> RBColor {
+    match node.as_ref() {
+        None => RBColor::Black,
+        Some(val) => {
+            val.borrow().color
+        }
+    }
+}
+
+fn get_key<T: std::cmp::PartialOrd + Copy>(node: &StrongNodeRef<T>) -> Option<T> {
+    match node.as_ref() {
+        None => None,
+        Some(val) => {
+            Some(val.borrow().key)
+        }
+    }
+}
+
+fn set_left<T: std::cmp::PartialOrd + Copy>(node: &StrongNodeRef<T>, left: StrongNodeRef<T>) {
+    node.as_ref().map(|val| {
+        val.borrow_mut().left = left;
+    });
+}
+
+fn set_right<T: std::cmp::PartialOrd + Copy>(node: &StrongNodeRef<T>, right: StrongNodeRef<T>) {
+    node.as_ref().map(|val| {
+        val.borrow_mut().right = right;
+    });
+}
+
+fn set_parent<T: std::cmp::PartialOrd + Copy>(node: &StrongNodeRef<T>, parent: WeakNodeRef<T>) {
+    node.as_ref().map(|val| {
+        val.borrow_mut().p = parent;
+    });
+}
+
+fn set_color<T: std::cmp::PartialOrd + Copy>(node: &StrongNodeRef<T>, color: RBColor) {
+    node.as_ref().map(|val| {
+        val.borrow_mut().color = color;
+    });
+}
+
 fn to_weak<T: std::cmp::PartialOrd + Copy>(node: &StrongNodeRef<T>) -> WeakNodeRef<T> {
     match node.as_ref() {
         None => None,
@@ -104,7 +143,7 @@ fn to_strong<T: std::cmp::PartialOrd + Copy>(node: &WeakNodeRef<T>) -> StrongNod
             Some(Weak::upgrade(val).expect("INVALID STATE!"))
         }
     }
-}
+}    
 
 impl<K: std::cmp::PartialOrd + Copy> PartialEq<RBNode<K>> for RBNode<K> {
     fn eq(&self, other: &Self) -> bool {
@@ -112,12 +151,14 @@ impl<K: std::cmp::PartialOrd + Copy> PartialEq<RBNode<K>> for RBNode<K> {
     }
 } 
 
-impl<K: std::cmp::PartialOrd + Copy> RBTree<K> {
+impl<K: std::cmp::PartialOrd + Copy + Debug> RBTree<K> {
     fn new() -> RBTree<K> {
         RBTree {root: None}
     }
 
     fn insert(&mut self, key: K) -> &mut Self {
+        // println!("Inserting {:?}!", key);
+        // self.print();
         let mut y: StrongNodeRef<K> = None;
         let mut x: StrongNodeRef<K> = clone_node(&self.root);
         while let Some(rc_node) = clone_node(&x) {
@@ -148,12 +189,97 @@ impl<K: std::cmp::PartialOrd + Copy> RBTree<K> {
                 }
             }
         }
-        self.insert_fixup(&mut z_node);
+        
+        self.insert_fixup(z_node);
         return self;
     }
 
-    fn insert_fixup(&mut self, x: &mut StrongNodeRef<K>) {
+/*
+RB-Insert-fixup(T,z)
+  while color[p[z]] = RED {
+    if p[z] == left[p[p[z]]] {
+         y = right[p[p[z]]]
+         if color[y] = RED {
+             color[p[z]] = BLACK
+             color[y] = BLACK
+             color[p[p[z]]] = RED
+             z = p[p[z]]
+         }
+         else {
+             if z = right[p[z]] {
+                  z = p[z]
+                  LEFT-Rotate(T,z)
+             }
+             color[p[z]] = BLACK
+             color[p[p[z]]] = RED
+             RIGHT-Rotate(T,p[p[z]])
+         }
+    }
+    else {
+         y = left[p[p[z]]]
+         if color[y] = RED {
+             color[p[z]] = BLACK
+             color[y] = BLACK
+             color[p[p[z]]] = RED
+             z = p[p[z]]
+         }
+         else
+             {
+             if z = left[p[z]] {
+                  z = p[z]
+                  RIGHT-Rotate(T,z)
+             }
+             color[p[z]] = BLACK
+             color[p[p[z]]] = RED
+             LEFT-Rotate(T,p[p[z]])
+         }
+    }
+    color[root[T]] = BLACK
+  }
+*/
 
+    fn insert_fixup(&mut self, mut z: StrongNodeRef<K>) {
+        while get_color(&to_strong(&get_parent(&z))) == RBColor::Red {
+            // println!("\nAttempted fixup for where k={:?}.", get_key(&z).unwrap());
+            // self.print();
+            if to_strong(&get_parent(&z)) == get_left(&to_strong(&get_parent(&to_strong(&get_parent(&z))))) {
+                /* If the parent is to the left of the grandparent */
+                /* y is z's uncle */
+                let y = get_right(&to_strong(&get_parent(&to_strong(&get_parent(&z)))));
+                if get_color(&y) == RBColor::Red {
+                    set_color(&to_strong(&get_parent(&z)), RBColor::Black);
+                    set_color(&y, RBColor::Black);
+                    set_color(&to_strong(&get_parent(&to_strong(&get_parent(&z)))), RBColor::Red);
+                    z = to_strong(&get_parent(&to_strong(&get_parent(&z))));
+                } else {
+                    if z == get_right(&to_strong(&get_parent(&z))) {
+                        z = to_strong(&get_parent(&z));
+                        self.left_rotate(&mut z);
+                    }
+                    set_color(&to_strong(&get_parent(&z)), RBColor::Black);
+                    set_color(&to_strong(&get_parent(&to_strong(&get_parent(&z)))), RBColor::Red);
+                    self.right_rotate(&mut to_strong(&get_parent(&to_strong(&get_parent(&z)))));
+                }
+            } else {
+                /* If the parent is to the right of the grandparent */
+                let y = get_left(&to_strong(&get_parent(&to_strong(&get_parent(&z)))));
+                if get_color(&y) == RBColor::Red {
+                    set_color(&to_strong(&get_parent(&z)), RBColor::Black);
+                    set_color(&y, RBColor::Black);
+                    set_color(&to_strong(&get_parent(&to_strong(&get_parent(&z)))), RBColor::Red);
+                    z = to_strong(&get_parent(&to_strong(&get_parent(&z))));
+                } else {
+                    if z == get_left(&to_strong(&get_parent(&z))) {
+                        z = to_strong(&get_parent(&z));
+                        self.right_rotate(&mut z);
+                    }
+                    set_color(&to_strong(&get_parent(&z)), RBColor::Black);
+                    set_color(&to_strong(&get_parent(&to_strong(&get_parent(&z)))), RBColor::Red);
+                    self.left_rotate(&mut to_strong(&get_parent(&to_strong(&get_parent(&z)))));
+                }
+            }
+        }
+        set_color(&self.root, RBColor::Black);
     }
 
 /*
@@ -177,35 +303,23 @@ Left-Rotate(T,x)
             return;
         }
         let mut y = get_right(x);
-        x.as_ref().map(|rc| {
-            rc.borrow_mut().right = clone_node(&get_left(&y));
-        });
-        get_left(&y).as_mut().map(|rc| {
-            rc.borrow_mut().p = to_weak(&x);
-        });
-        y.as_mut().map(|rc| {
-            rc.borrow_mut().p = get_parent(&x);
-        });
+        assert!(y.is_some());
+        set_right(x, clone_node(&get_left(&y)));
+        set_parent(&get_left(&y), to_weak(&x));
+        set_parent(&get_left(&y), to_weak(&x));
+        set_parent(&y, get_parent(&x));
 
         if get_parent(x).is_none() {
             self.root = clone_node(&y);
         } else {
             if x == &mut get_left(&to_strong(&get_parent(x))) {
-                to_strong(&get_parent(&x)).map(|rc| {
-                    rc.borrow_mut().left = clone_node(&y);
-                });
+                set_left(&to_strong(&get_parent(&x)), clone_node(&y));
             } else {
-                to_strong(&get_parent(&x)).map(|rc| {
-                    rc.borrow_mut().right = clone_node(&y);
-                });
+                set_right(&to_strong(&get_parent(&x)), clone_node(&y));
             }
         }
-        y.as_mut().map(|rc| {
-            rc.borrow_mut().left = clone_node(&x);
-        });
-        x.as_mut().map(|rc| {
-            rc.borrow_mut().p = to_weak(&y);
-        });
+        set_left(&y, clone_node(&x));
+        set_parent(&x, to_weak(&y));
     }
 
     fn right_rotate(&mut self, x: &mut StrongNodeRef<K>) {
@@ -213,58 +327,57 @@ Left-Rotate(T,x)
             return;
         }
         let mut y = get_left(x);
-        x.as_ref().map(|rc| {
-            rc.borrow_mut().left = clone_node(&get_right(&y));
-        });
-        get_right(&y).as_mut().map(|rc| {
-            rc.borrow_mut().p = to_weak(&x);
-        });
-        y.as_mut().map(|rc| {
-            rc.borrow_mut().p = get_parent(&x);
-        });
+        assert!(y.is_some());
+        set_left(x, clone_node(&get_right(&y)));
+        set_parent(&get_right(&y), to_weak(&x));
+        set_parent(&get_right(&y), to_weak(&x));
+        set_parent(&y, get_parent(&x));
 
         if get_parent(x).is_none() {
             self.root = clone_node(&y);
         } else {
             if x == &mut get_left(&to_strong(&get_parent(x))) {
-                to_strong(&get_parent(&x)).map(|rc| {
-                    rc.borrow_mut().left = clone_node(&y);
-                });
+                set_left(&to_strong(&get_parent(&x)), clone_node(&y));
             } else {
-                to_strong(&get_parent(&x)).map(|rc| {
-                    rc.borrow_mut().right = clone_node(&y);
-                });
+                set_right(&to_strong(&get_parent(&x)), clone_node(&y));
             }
         }
-        y.as_mut().map(|rc| {
-            rc.borrow_mut().right = clone_node(&x);
-        });
-        x.as_mut().map(|rc| {
-            rc.borrow_mut().p = to_weak(&y);
-        });
+        set_right(&y, clone_node(&x));
+        set_parent(&x, to_weak(&y));
     }
 }
 
 const INDENT: &str = "  ";
+#[derive(Copy, Clone, PartialEq)]
+enum node_type {ROOT, LEFT, RIGHT}
 impl<K: std::cmp::PartialOrd + Copy + Debug> RBTree<K> {
     fn print(&self) {
-        Self::print_internal(&self.root, 0, false);
+        let s = String::from("");
+        Self::print_internal(&self.root, 0, node_type::ROOT, s);
     }
-
-    fn print_internal(node: &StrongNodeRef<K>, indent: u32, left_branch: bool) {
+    
+    fn print_internal(node: &StrongNodeRef<K>, indent: u32, nt: node_type, s: String) {
         if let Some(rf) = node {
             let left_node = &rf.borrow().left;
             let right_node = &rf.borrow().right;
-
-            let (le, re) = (left_node.is_some(), right_node.is_some());
-
-            Self::print_internal(right_node, indent + 1, false);
-
-            for _ in 0..(std::cmp::max(indent, 1)-1) {
-                print!("{}", INDENT);
+            let mut lft_str = s.clone();
+            let mut rgt_str = s.clone();
+            if nt == node_type::ROOT {
+                lft_str.push_str("  ");
+                rgt_str.push_str("  ");
+            } else if nt == node_type::LEFT {
+                lft_str.push_str("  ");
+                rgt_str.push_str("| ");
+            } else {
+                lft_str.push_str("| ");
+                rgt_str.push_str("  ");
             }
+
+            Self::print_internal(right_node, indent + 1, node_type::RIGHT, rgt_str);
+
+            print!("{}", s);
             if 0 < indent {
-                if left_branch {
+                if nt == node_type::LEFT {
                     print!("└ ");
                 } else {
                     print!("┌ ");
@@ -272,13 +385,13 @@ impl<K: std::cmp::PartialOrd + Copy + Debug> RBTree<K> {
             }
             println!("{} {:?} ct:{}", 
                 match rf.borrow().color {
-                    RBColor::Black => "(Black)", 
-                    RBColor::Red => "(Red)"
+                    RBColor::Black => "B", 
+                    RBColor::Red => "R"
                 }, 
                 rf.borrow().key,
                 Rc::strong_count(rf)
             );
-            Self::print_internal(left_node, indent + 1, true);
+            Self::print_internal(left_node, indent + 1, node_type::LEFT, lft_str);
         }
     }
 }
@@ -291,16 +404,12 @@ impl<T: std::cmp::PartialOrd + Copy> Drop for RBTree<T> {
 
 fn main() {
     let mut tree = RBTree::<i32>::new();
-    tree.insert(12);
-    tree.insert(10);
-    tree.insert(11);
-    tree.insert(20);
+    for i in 1..100 {
+        tree.insert(i);
+    }
     tree.print();
     println!();
-    let mut root = &mut clone_node(&tree.root);
-    tree.left_rotate(&mut root);
-    tree.print();
-    println!();
-    tree.right_rotate(&mut root);
-    tree.print();
+
+
+
 }
